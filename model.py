39a225e6ad0da42c11545012379ae9675641a001
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-04-08 19:55:16 +0300

    добавил block

--- model.py
+++ model.py
@@ -173,3 +173,18 @@ class MLP(nn.Module):
         x = self.c_proj(x)
         return self.dropout(x)
 
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        assert not torch.isnan(x).any(), "Обнаружены NaN в активациях"
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
